a27ce43f5eb076a93138171880c4dceb4db45b08
reivieir <reinaldo.paulo@gmail.com>
2026-02-08 00:40:35 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,14 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
+  const env = loadEnv(mode, '.', '');
+  return {
+    base: '/', // <--- ADICIONE OU ALTERE PARA ESTA LINHA
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+    },
+    plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
