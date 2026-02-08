acee8291c386e7cf17e047964f6c2c54228e55b0
reivieir <reinaldo.paulo@gmail.com>
2026-02-08 10:53:59 -0300

    Update geminiService.ts

--- services/geminiService.ts
+++ services/geminiService.ts
@@ -1,13 +1,12 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { PERGUNTAS_FREQUENTES } from "../constants";
 
-const API_KEY = import.meta.env.VITE_GEMINI_API_KEY || '';
-const genAI = new GoogleGenerativeAI(API_KEY);
+const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY || '');
 
 export const createDexcoChat = async () => {
   const model = genAI.getGenerativeModel({ 
     model: "gemini-1.5-flash",
-    systemInstruction: "Você é o Dexco Assist. Responda com base nisto: " + JSON.stringify(PERGUNTAS_FREQUENTES)
+    systemInstruction: "Você é o Dexco Assist. Responda formalmente com base nisto: " + JSON.stringify(PERGUNTAS_FREQUENTES)
   });
   return model.startChat({ history: [] });
 };
