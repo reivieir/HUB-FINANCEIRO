bc6bcc5c58f2ce672ec20fdaa86e567a17f3eb4a
reivieir <reinaldo.paulo@gmail.com>
2026-02-09 16:31:58 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,17 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// ... mantenha os imports iguais
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-    base: './', 
-    server: {
-      port: 3000,
-      host: '0.0.0.0',
+export default defineConfig({
+  base: './',
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
     },
-      plugins: [react()],
-      define: {
-        // Isso garante que qualquer chamada a 'process.env' não quebre o site
-        'process.env': {} 
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+  },
 });
