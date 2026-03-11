ea33cea370b1a1dd41c0cac11efbdd52aa0965a8
reivieir <reinaldo.paulo@gmail.com>
2026-03-11 10:31:11 -0300

    Create Conciliacao.tsx

---
+++ components/Conciliacao.tsx
@@ -0,0 +1,11 @@
+export default function Conciliacao() {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold mb-4">Conciliação Bancária</h1>
+
+      <p>
+        Nesta seção serão disponibilizadas as contas bancárias e contas contábeis para consulta.
+      </p>
+    </div>
+  );
+}
