e08554afec2808afc0919ddc4a63fd024f145b3f
reivieir <reinaldo.paulo@gmail.com>
2026-03-12 10:40:14 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,12 +1,10 @@
 import { useState } from "react";
 import Sidebar from "./components/Sidebar";
-
 import AssistenteIA from "./components/AssistenteIA";
 import Bancos from "./components/Bancos";
 import Conciliacao from "./components/Conciliacao";
 
 function App() {
-
   const [pagina, setPagina] = useState("ia");
 
   const renderPagina = () => {
@@ -23,13 +21,10 @@ function App() {
 
   return (
     <div className="flex">
-
       <Sidebar setPagina={setPagina} />
-
       <div className="flex-1 bg-[#121212] text-white min-h-screen">
         {renderPagina()}
       </div>
-
     </div>
   );
 }
