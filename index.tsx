9fd69883d53407581cbe465200e512f6d2ac42b6
reivieir <reinaldo.paulo@gmail.com>
2026-03-12 11:13:31 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css' // Certifique-se que este arquivo existe ou remova a linha
+// Se você tiver um arquivo CSS global, importe-o aqui. Ex: import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
